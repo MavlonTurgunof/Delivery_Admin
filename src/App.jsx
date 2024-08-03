5c7c933d57791a5c31da2d7a50fd108dc664d0da
MavlonTurgunof <mavlondev2023@gmail.com>
2024-08-03 23:37:42 +0500

    Pages are created Product create page was builded and connected to the backend fully functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "../src/components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <ToastContainer />
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/orders" element={<Orders />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/List/List.jsx
@@ -0,0 +1,7 @@
+import "./List.css";
+
+function List() {
+  return <div></div>;
+}
+
+export default List;
